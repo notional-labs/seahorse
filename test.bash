7eb749363fdf06b1385c6488e04b269c78ae3c20
Jacob Gadikian <jacobgadikian@gmail.com>
2020-11-17 16:28:51 +0700

    Update test.bash

--- test.bash
+++ test.bash
@@ -11,7 +11,11 @@ set -euo pipefail
 
 
 fallocate -l 2G "custom-pi.img"
-export DEVICE = $(sudo losetup --find --show "custom-pi.img")
+
+
+export device=$(sudo losetup --find --show "custom-pi.img")
+
+
 sudo parted --script /dev/loop0 mklabel msdos
 sudo parted --script /dev/loop0 mkpart primary fat32 0% 200M
 sudo parted --script /dev/loop0 mkpart primary ext4 100M 100%
@@ -36,7 +40,7 @@ sudo cp /usr/bin/qemu-arm-static /mnt/usr/bin/
 sudo chroot /mnt /bin/bash <<"EOT"
 
 pacman -Syyu vim bash-completion
-echo custom-pi > /etc/hostname
+echo starport-pi > /etc/hostname
 
 sed -i "s/alarm/pi/g" /etc/passwd /etc/group /etc/shadow
 mv /home/alarm "/home/pi"
