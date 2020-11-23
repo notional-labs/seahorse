9d15c96f3d82e96b08524e0f4bda0d01f6e73753
Jacob Gadikian <jacobgadikian@gmail.com>
2020-11-23 16:28:00 +0700

    Update firstboot.sh

--- contrib/firstboot.sh
+++ contrib/firstboot.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
 rm -f /etc/ssh/ssh_host_*
-ssh-keygen -v -A
-
-# There is no longer a /dev/mmcblk0p3
-# umount /dev/mmcblk0p3
-# parted /dev/mmcblk0 -a optimal -s resizepart 3 100%
-# yes | mkfs.ext4 -F -m 0 /dev/mmcblk0p3
-# mount /dev/mmcblk0p3
 
 # Set Time Zone
 # Later, make this automatic based on location.
