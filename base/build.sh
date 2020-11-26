d0903c1fb0e3bf400300fb81fbcea2d910810d62
Jacob Gadikian <jacobgadikian@gmail.com>
2020-11-26 21:54:26 +0700

    Update build.sh
    base

--- base/build.sh
+++ base/build.sh
@@ -1,7 +1,98 @@
 #!/bin/bash
 
+# Fail on error
+set -exo pipefail
+
+# Print each command
+set -o xtrace
+
 # Get the 64 bit rpi rootfs for Pi 3 and 4
 wget -N --progress=bar:force:noscroll http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-aarch64-latest.tar.gz
 
 # Build the base image
-docker buildx build --tag faddat/sos-base --platform linux/arm64 --push --cache-from faddat/sos-base:cache --cache-to faddat/sos-base:cache --progress plain .
+docker buildx build --tag faddat/sos-base --platform linux/arm64 --load --cache-from faddat/sos-base:cache --cache-to faddat/sos-base:cache --progress plain .
+
+# TAG AND PUSH
+docker tag sos faddat/sos-base
+docker push faddat/sos-base
+
+# PREPARE TOOLBOX
+# docker buildx build --rm --tag toolbox --file toolbox/Dockerfile.root --load  --progress plain toolbox
+
+# EXTRACT IMAGE
+# Make a temporary directory
+rm -rf .tmp | true
+mkdir .tmp
+
+# remove anything in the way of extraction
+docker run --rm --tty --volume $(pwd)/./.tmp:/root/./.tmp --workdir /root/./.tmp/.. faddat/toolbox rm -rf ./.tmp/result-rootfs
+
+# save the image to result-rootfs.tar
+docker save --output ./.tmp/result-rootfs.tar sos-base
+
+# Extract the image using docker-extract
+docker run --rm --tty --volume $(pwd)/./.tmp:/root/./.tmp --workdir /root/./.tmp/.. faddat/toolbox /tools/docker-extract --root ./.tmp/result-rootfs  ./.tmp/result-rootfs.tar
+
+# Set hostname while the image is just in the filesystem.
+sudo bash -c "echo sos > ./.tmp/result-rootfs/etc/hostname"
+
+
+# ===================================================================================
+# IMAGE: Make a .img file and compress it.
+# Uses Techniques from Disconnected Systems:
+# https://disconnected.systems/blog/raspberry-pi-archlinuxarm-setup/
+# ===================================================================================
+
+
+# Unmount anything on the loop device
+sudo umount /dev/loop0p2 || true
+sudo umount /dev/loop0p1 || true
+
+
+# Detach from the loop device
+sudo losetup -d /dev/loop0 || true
+
+# Unmount anything on the loop device
+sudo umount /dev/loop0p2 || true
+sudo umount /dev/loop0p1 || true
+
+
+# Create a folder for images
+rm -rf images || true
+mkdir -p images
+
+# Make the image file
+fallocate -l 2G "images/sos-base.img"
+
+# loop-mount the image file so it becomes a disk
+sudo losetup --find --show images/sos-base.img
+
+# partition the loop-mounted disk
+sudo parted --script /dev/loop0 mklabel msdos
+sudo parted --script /dev/loop0 mkpart primary fat32 0% 200M
+sudo parted --script /dev/loop0 mkpart primary ext4 200M 100%
+
+# format the newly partitioned loop-mounted disk
+sudo mkfs.vfat -F32 /dev/loop0p1
+sudo mkfs.ext4 -F /dev/loop0p2
+
+# Use the toolbox to copy the rootfs into the filesystem we formatted above.
+# * mount the disk's /boot and / partitions
+# * use rsync to copy files into the filesystem
+# make a folder so we can mount the boot partition
+# soon will not use toolbox
+
+sudo mkdir -p mnt/boot mnt/rootfs
+sudo mount /dev/loop0p1 mnt/boot
+sudo mount /dev/loop0p2 mnt/rootfs
+sudo rsync -a ./.tmp/result-rootfs/boot/* mnt/boot
+sudo rsync -a ./.tmp/result-rootfs/* mnt/rootfs --exclude boot
+sudo mkdir mnt/rootfs/boot
+sudo umount mnt/boot mnt/rootfs
+
+# Tell pi where its memory card is:  This is needed only with the mainline linux kernel provied by linux-aarch64
+# sed -i 's/mmcblk0/mmcblk1/g' ./.tmp/result-rootfs/etc/fstab
+
+# Drop the loop mount
+sudo losetup -d /dev/loop0
+
