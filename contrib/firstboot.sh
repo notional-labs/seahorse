1b4eb5d41507304810dcf9bfa5254342290a4832
Jacob Gadikian <jacobgadikian@gmail.com>
2021-03-21 16:46:18 +0700

    Update firstboot.sh

--- contrib/firstboot.sh
+++ contrib/firstboot.sh
@@ -7,8 +7,5 @@ timedatectl set-timezone UTC
 # mdns
 systemd-resolve --set-mdns=yes --interface=eth0
 
-# install go and npm
-pacman -Syyu --noconfirm npm go
-
 # Don't run again
 systemctl disable firstboot
